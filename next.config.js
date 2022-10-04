8181d16e27bccdb719c1c41008181b72981fc809
Sebastian Scherbes <github@scherbes.de>
2022-10-04 17:39:04 +0200

    include image from ext domain

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: { domains: ["unsplash.com"] },
+  compiler: {
+    styledComponents: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- pages/dessert.js
+++ pages/dessert.js
@@ -1,8 +1,16 @@
+import Image from "next/image";
+
 export default function Dessert() {
   return (
     <>
       <h1>Dessert</h1>
-      <p>coming soon...</p>
+      <Image
+        src="https://unsplash.com/photos/surQ2mkZNxw/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8Mzd8fGRlc3NlcnR8ZW58MHwwfHx8MTY1OTMzNzk1NQ&force=true&w=2400"
+        alt="bla"
+        layout="responsive"
+        width={2400}
+        height={1599}
+      />
     </>
   );
 }
